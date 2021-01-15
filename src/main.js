9c2e4e3423bfab1e744193c0d3f43b8490c53908
yijutuan <yijutuan@gmail.com>
2021-01-15 09:45:57 -0800

    LCD-9904 fix createTable and import multiple documents

--- src/main.js
+++ src/main.js
@@ -5,21 +5,44 @@ const get = require('get-value');
 const config = require('../config/config');
 
 const DATABASE_NAME = 'testimport';
-const ORIGINAL_COLUMNS = ['id', 'projectId'];
-const ADDED_COLUMNS = ['organizationId'];
+
+const ORIGINAL_COLUMNS = {
+  'id': {
+    type: 'text',
+    constraints: ['PRIMARY KEY'],
+  },
+  'projectId': {
+    type: 'text',
+    constraints: ['NOT NULL'],
+  },
+};
+
+const ADDED_COLUMNS = {
+  'organizationId': {
+    type: 'text',
+    constraints: ['NOT NULL'],
+  },
+};
 
 async function runImportPostgre() {
+  const allColumns = {
+    ...ORIGINAL_COLUMNS,
+    ...ADDED_COLUMNS,
+  };
+
   console.log('Connecting to Postgresql');
   const pool = new Pool({...config.postgres});
   console.log('Connected to Postgresql');
+
+  await createTable(pool, allColumns);
 
   const mongodbURL = createMongoURL(
     config.mongodbURL,
     config.mongodbUser,
     config.mongodbPassword
   );
-  console.log('Connecting to Mongodb');
 
+  console.log('Connecting to Mongodb');
   try {
     const client = await MongoClient.connect(mongodbURL, {
       ignoreUndefined: true,
@@ -27,20 +50,31 @@ async function runImportPostgre() {
       useUnifiedTopology: true,
     });
     console.log('Connected to Mongodb');
+  } catch (error) {
+    console.error('Error when connecting to mongodb and updating', error);
+    pool.end();
+    return;
+  }
 
+  try {
     const mongodb = client.db();
     const activitiesCollection = await mongodb.collection('activities');
     const cursor = await activitiesCollection.find({}).batchSize(10000);
     let record;
 
+    let number = 0;
     while((record = await cursor.next())) {
       console.log(record);
-      await insert(pool, DATABASE_NAME, record);
-      break; // just to test one record at a time
+      await insert(pool, DATABASE_NAME, allColumns, record);
+      number++;
+      if (number >= 10) {
+        break;
+      }
     }
 
   } catch (error) {
-    console.error('Error connecting to mongodb', error);
+    console.error('Error when getting value from mongodb and updating to postgres', error);
+    pool.end();
     return;
   }
 
@@ -48,8 +82,34 @@ async function runImportPostgre() {
 
   // const record = getValuesFromFile('test.json');
   // await insert(pool, DATABASE_NAME, record);
+  pool.end();
 }
 
+/**
+ * Create Table in Postgres
+ * @param {Pool} pool
+ * @param {object} allColumns
+ */
+async function createTable(pool, allColumns) {
+  const columnStringArray = Object.keys(allColumns).map(colName => {
+    const type = allColumns[colName].type;
+    const constraints = allColumns[colName].constraints ?
+      allColumns[colName].constraints.join(' ')
+      : '';
+    return `${colName} ${type} ${constraints}`;
+  });
+
+  const columnsString = createColumnsString(columnStringArray);
+  const query = `CREATE TABLE ${DATABASE_NAME} ${columnsString};`;
+
+  console.log(query);
+  await runSqlQuery(pool, query);
+}
+
+/**
+ * Gets value from a JSON file. This is used for testing purposes
+ * @param {string} file - name of file in mongodb-docs
+ */
 function getValuesFromFile(file) {
   const record = require(`../mongodb-docs/${file}`);
   return record;
@@ -58,33 +118,40 @@ function getValuesFromFile(file) {
 /**
  * Insert Values from record into Postgresql
  * @param {Pool} pool 
+ * @param {string} databaseName
+ * @param {allColumns} object
  * @param {Object} record 
  */
-async function insert(pool, databaseName, record) {
-  const columnsList = ORIGINAL_COLUMNS;
-
-  addOnAnalysisColumns(columnsList);
-  const valuesString = createValuesString(record, columnsList);
+async function insert(pool, databaseName, allColumns, record) {
+  const columnsList = Object.keys(allColumns);
   const columnsString = createColumnsString(columnsList);
+  const valuesString = createValuesString(record, columnsList);
 
   const query = 
     `INSERT INTO ${databaseName} ${columnsString} `
     + `VALUES (${valuesString})`;
   console.log('query: ', query);
+  await runSqlQuery(pool, query);
+}
+
+/**
+ * Runs the Sql Query
+ * @param {Pool} pool
+ * @param {string} query
+ */
+async function runSqlQuery(pool, query) {
   try {
     const result = await pool.query(query);
     console.log(result);
   } catch (error) {
     console.log('Could not insert into postgres', error);
+    throw error;
   }
-  pool.end();
 }
 
-function addOnAnalysisColumns(columnsList) {
-  const newColumns = ADDED_COLUMNS;
-  columnsList.push(...newColumns);
-}
-
+/**
+ * Create string representing columns.
+ */
 function createColumnsString(columnsList) {
   return '(' + columnsList.join(', ') + ')';
 }
